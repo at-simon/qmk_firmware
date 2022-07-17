d262ae8d684731aeda6a23615656d59195607c6c
simons-repos <109457359+simons-repos@users.noreply.github.com>
2022-07-17 15:48:00 +0200

    initial commit

--- keyboards/kbdfans/kbdpad/mk2/keymaps/via/keymap.c
+++ keyboards/kbdfans/kbdpad/mk2/keymaps/via/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
+1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_6x4( /* Base */
