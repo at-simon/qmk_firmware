d1015c6ee92c54e240f6390b319d14dfeb64c19c
simons-repos <109457359+simons-repos@users.noreply.github.com>
2022-07-17 13:00:37 +0200

    renaming

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/at-simon/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/at-simon/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 simons-repos
+/* Copyright 2022 at-simon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
